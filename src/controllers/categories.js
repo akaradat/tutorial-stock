9f7513e8a18f3fff597988ba85bcecccf7cea8bb
KIITTIKUN <kittikun.real@gmail.com>
2022-09-15 15:49:08 +0700

    feat(categories): get category by id API

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,3 +1,17 @@
 import HttpStatus from 'http-status-codes';
 
 import * as categoriesService from '../services/categoriesService';
+
+/**
+ * Get a categories by its id.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+export function fetchById(req, res, next) {
+  categoriesService
+    .getCategories(req.params.id)
+    .then((data) => res.json({ data }))
+    .catch((err) => next(err));
+}
--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -1,3 +1,18 @@
 import Boom from '@hapi/boom';
 
 import Categories from '../models/categories';
+
+/**
+ * Get a Categories.
+ *
+ * @param   {Number|String}  id
+ * @returns {Promise}
+ */
+export function getCategories(id) {
+  return new Categories({ id })
+    .fetch()
+    .then((category) => category)
+    .catch(Categories.NotFoundError, () => {
+      throw Boom.notFound('Category not found');
+    });
+}
