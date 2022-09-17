7e4bb097985befa3a1c30301e335659d4839ef59
KIITTIKUN <kittikun.real@gmail.com>
2022-09-17 22:41:49 +0700

    feat(): delete product API

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,3 +1,28 @@
 import Boom from '@hapi/boom';
 
 import Product from '../models/product';
+
+/**
+ * Get a Product.
+ *
+ * @param   {Number|String}  id
+ * @returns {Promise}
+ */
+export function getProduct(id) {
+  return new Product({ id })
+    .fetch()
+    .then((product) => product)
+    .catch(Product.NotFoundError, () => {
+      throw Boom.notFound('Product not found');
+    });
+}
+
+/**
+ * Delete a product.
+ *
+ * @param   {Number|String}  id
+ * @returns {Promise}
+ */
+export function deleteProduct(id) {
+  return new Product({ id }).fetch().then((product) => product.destroy());
+}
